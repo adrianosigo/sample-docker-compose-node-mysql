147450ad69f360f25ca2e16e380328070763df19
Adriano Gomes <ag@mlike.com.br>
2023-04-07 23:46:35 -0300

    Normalization username and database

--- node/database/database.js
+++ node/database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('nodedb', 'dbuser', '123456', {
+const connection = new Sequelize('db_node', 'db_user', '123456', {
     host: 'db',
     dialect: 'mysql'
 });
